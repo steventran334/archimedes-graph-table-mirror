d5ba764ea9405f84f46f190f1aa098493fd9ec69
supersausage <65040350+steventran334@users.noreply.github.com>
2026-02-12 12:43:53 -0500

    Update archimedesfinalapp.py

--- archimedesfinalapp.py
+++ archimedesfinalapp.py
@@ -69,20 +69,20 @@ def render_table_as_figure(df, title="Summary Table", col_width=3.0, row_height=
 raw_uploaded_files = st.file_uploader("Upload one or more CSV files", type="csv", accept_multiple_files=True)
 
 if raw_uploaded_files:
-        # --- FILE REORDERING FEATURE ---
-        # Map filenames to file objects
-        file_dict = {f.name: f for f in raw_uploaded_files}
-        file_names = list(file_dict.keys())
-        
-        st.subheader("Step 1: Define File Order & Selection")
-        
-        # Changed default=file_names to default=None to prevent automatic selection
-        ordered_filenames = st.multiselect(
-            "Select files in the order you want them to appear (click to add)",
-            options=file_names,
-            default=None,  
-            help="Files will appear in the legend and table in the order you select them here."
-        )
+    # --- FILE REORDERING FEATURE ---
+    # Map filenames to file objects
+    file_dict = {f.name: f for f in raw_uploaded_files}
+    file_names = list(file_dict.keys())
+    
+    st.subheader("Step 1: Define File Order & Selection")
+    
+    # Changed default=file_names to default=None to prevent automatic selection
+    ordered_filenames = st.multiselect(
+        "Select files in the order you want them to appear (click to add)",
+        options=file_names,
+        default=None,  
+        help="Files will appear in the legend and table in the order you select them here."
+    )
         
         # Create the final list based on user selection
         uploaded_files = [file_dict[name] for name in ordered_filenames]
